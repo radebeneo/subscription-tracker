fe0dcde468085c7b180f427c47a255566b1a4e17
radebeneo <radebeneo17@gmail.com>
2025-12-27 14:01:06 +0200

    completed test sign-up requests.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
-import { signUp, signIn, signOut } from '../controllers/auth.controller';
+import { signUp, signIn, signOut } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
-authRouter.get('/sign-up', signUp)
-authRouter.get('/sign-in', signIn)
-authRouter.get('/sign-out',  signOut)
+authRouter.post('/sign-up', signUp)
+authRouter.post('/sign-in', signIn)
+authRouter.post('/sign-out',  signOut)
 
 export default authRouter;
