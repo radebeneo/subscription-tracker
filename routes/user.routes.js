09bc94bdaed200ae960e600b1b423739abe90791
radebeneo <radebeneo17@gmail.com>
2025-12-28 21:46:20 +0200

    added auth guard middleware for user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
+
+import authorize from '../middlewares/auth.middleware.js';
 import {getUserById, getUsers} from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers)
 
-userRouter.get('/:id', getUserById)
+userRouter.get('/:id', authorize, getUserById)
 
 userRouter.post('/', (req, res) => res.send({title: 'CREATE new users'}))
 
