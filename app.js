1fdbea92fadc05f60611de5d9b9473ab0332e139
radebeneo <radebeneo17@gmail.com>
2025-12-31 00:53:34 +0200

    completed arcjet functionality to protect app against bots and rate limiting (spamming)

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from "./routes/auth.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 
 const app = express();
@@ -16,11 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.use(errorMiddleware)
+app.use(arcjetMiddleware)
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Subscription Tracker API!');
--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -1,19 +1,27 @@
 import aj from '../config/arcjet.js'
 
-const arcjetMiddleware = (req, res, next) => {
+const arcjetMiddleware = async (req, res, next) => {
     try{
-        const decision = await aj.protect(req);
+        const decision = await aj.protect(req, {requested: 1});
 
         if (decision.isDenied()) {
 
-            if (decision.reason.isRateLimit()) return res.status(429).json({message: 'Rate limit reached'});
+            if (decision.reason.isRateLimit()) return res.status(429).json({error: 'Rate limit reached'});
 
-            if(decision.reason.isBot()) return res.status(403).json({message: 'Bot detected'});
+            if(decision.reason.isBot()) return res.status(403).json({error: 'Bot detected'});
+
+            return res.status(403).json({error: 'Access denied'});
 
         }
+
+        next()
+
     } catch (error){
         console.log(`Arcjet Middleware Error: ${error}`)
         next(error)
 
     }
 }
+
+
+export default arcjetMiddleware;
