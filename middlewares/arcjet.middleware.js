1d800861a76587f118b10d5e2ad143f5770d6c98
radebeneo <radebeneo17@gmail.com>
2025-12-31 00:12:25 +0200

    added bot detected  status code in arcjet middleware.

--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -5,9 +5,11 @@ const arcjetMiddleware = (req, res, next) => {
         const decision = await aj.protect(req);
 
         if (decision.isDenied()) {
-            if (decision.reason.isRateLimit()){
-                return res.status(429).json({message: 'Rate limit reached'});
-            }
+
+            if (decision.reason.isRateLimit()) return res.status(429).json({message: 'Rate limit reached'});
+
+            if(decision.reason.isBot()) return res.status(403).json({message: 'Bot detected'});
+
         }
     } catch (error){
         console.log(`Arcjet Middleware Error: ${error}`)
