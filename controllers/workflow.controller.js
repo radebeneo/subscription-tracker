a309d8c2432d87ccebf051f12ed55ddb8f30f8f9
radebeneo <radebeneo17@gmail.com>
2026-02-03 15:10:45 +0200

    completed workflow logic for subscription reminders.

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -6,6 +6,9 @@ const {serve} = require('@upstash/workflow/express')
 
 import Subscription from "../models/subscription.model.js";
 
+const REMINDERS = [7, 5, 2, 1]
+
+
 export const sendReminders  = serve(async(context) => {
     const { subscriptionId } = context.requestPayload;
     const subscription = await fetchSubscription(context, subscriptionId)
@@ -18,10 +21,31 @@ export const sendReminders  = serve(async(context) => {
         console.log(`Renewal date had passed for subscription ${subscriptionId}. Stopping workflow.`)
         return
     }
+
+    for(const daysBefore of REMINDERS) {
+        const reminderDate = renewalDate.subtract(daysBefore, 'day')
+
+        if(reminderDate.isAfter(dayjs())) {
+            await sleepUntilReminder(context, `Reminder-${daysBefore} days before `, reminderDate)
+        }
+
+        await triggerReminder(context, `Reminder-${daysBefore} days before`)
+    }
 })
 
 const fetchSubscription = async (context, subscriptionId) => {
     return await context.run('get subscription', () => {
         return Subscription.findById(subscriptionId).populate('user', 'name email');
+    })
+}
+
+const sleepUntilReminder = async (context, label, date) => {
+    console.log(`Sleeping until ${label} reminder at ${date}`)
+    await context.sleepUntil(label, date.toDate())
+}
+
+const triggerReminder = async (context, label) => {
+    return await context.run(label, () => {
+        console.log(`Triggering ${label} reminder`)
     })
 }
